ae5bbd6813100ebb51b3d513159a734c26e9b9e1
AlexeyLyutych <31928978+AlexeyLyutych@users.noreply.github.com>
2018-05-14 00:57:56 +0300

    Add files via upload

---
+++ Zoo/Zoo/Animal_Window.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Zoo.Models;
+using System.Data.Entity;
+
+namespace Zoo
+{
+    /// <summary>
+    /// Логика взаимодействия для Animal_Window.xaml
+    /// </summary>
+    public partial class Animal_Window : Window
+    {
+        ZooContex db;
+        public Animal_Window()
+        {
+            InitializeComponent();
+            db = new ZooContex();
+            db.Animals.Load();
+            AnimalList.ItemsSource = db.Animals.Local.ToBindingList();
+
+
+        }
+        private void phonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Animal p = (Animal)AnimalList.SelectedItem;
+            KindText.Text = p.KindOfAnimal;
+            NumberText.Text = Convert.ToString(p.Number);
+            AviaryText.Text = Convert.ToString(p.AviaryNum);
+            KeeperText.Text = Convert.ToString(p.KeeperID);
+            select_img.Source = new BitmapImage(new Uri(p.ImagePath, UriKind.Absolute)); ;
+        }
+    }
+}
---
+++ Zoo/Zoo/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Zoo.Models;
+
+namespace Zoo
+{
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+            ZooContex db = new ZooContex();
+            db.Employess.Load();
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Associate_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Employees employees = new Employees(this);
+
+            employees.Show();
+            
+            
+            
+
+
+
+        }
+
+        private void Animals_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Animal_Window animal = new Animal_Window();
+            animal.Show();
+        }
+    }
+}
